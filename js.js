d58af1785a2804825e918947c6a072c015a11ca1
gulkabakc <gulus2022@gmail.com>
2022-01-20 16:22:35 +0300

    finish

---
+++ js.js
@@ -0,0 +1,27 @@
+import ScrollReveal from 'scrollreveal';
+
+
+ScrollReveal().reveal('.anime-left',{
+    origin:left,
+    duration:1000,
+    distance:'25rem',
+    delay:300
+});
+ScrollReveal().reveal('.anime-right',{
+    origin:right,
+    duration:1000,
+    distance:'25rem',
+    delay:600
+});
+ScrollReveal().reveal('.anime-top',{
+    origin:top,
+    duration:1000,
+    distance:'25rem',
+    delay:300
+});
+ScrollReveal().reveal('.anime-bottom',{
+    origin:bottom,
+    duration:1000,
+    distance:'25rem',
+    delay:300
+});
